2a37e735235f9cda7d69373cbef2e8f2d469040a
Muhammad Waleed Shehbaz <waleed@de-coders.net>
2025-05-14 18:24:45 +0500

    update code

---
+++ js/store-items.js
@@ -0,0 +1,28 @@
+  const products = [
+    {
+      image: "images/shop-p1.webp",
+      alt: "Lipstick",
+      title: "Luxury Lipstick",
+      price: "Rs. 1,499"
+    },
+    {
+      image: "images/shop-p2.webp",
+      alt: "Miceller Water",
+      title: "Miceller Water",
+      price: "Rs. 800"
+    },
+    {
+      image: "images/shop-p3.webp",
+      alt: "Makeup Sponge",
+      title: "Makeup Sponge",
+      price: "Rs. 600"
+    },
+    {
+      image: "images/shop-p4.webp",
+      alt: "Peach Blush",
+      title: "Peach Blush",
+      price: "Rs. 1,499"
+    },
+  ];
+// Key value pairs: key as attribute and value as name i.e: 'name: sheeza'
+// JSON: JavaScript Object Notation, array of objects
